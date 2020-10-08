310ccbf9f558a38efa5882787a3259f9d00980d1
Ihor Bezbabnykh <Ihor_Bezbabnykh@epam.com>
2020-10-08 16:54:26 +0300

    updated Tabs component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,64 +2,15 @@ import React, { Component } from 'react';
 
 import './app.scss';
 
-import ApiService from '../../services/api-service';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import Tabs from '../tabs';
-import Spinner from '../spinner';
-import ErrorIndicator from '../error-indicator';
 import ErrorBoundary from '../error-boundary';
 
 export default class App extends Component {
 
-    apiService = new ApiService();
-
     state = {
-        questionData: [],
-        filter: '',
-        currentActiveIndex: 0,
-        loading: true,
-        error: false
-    }
-
-    componentDidMount() {
-        this.updateData();
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        if (this.state.filter !== prevState.filter) {
-            this.setState({
-                loading: true
-            });
-            this.updateData();
-        }
-    }
-
-    updateData() {
-        this.apiService
-            .getData('5f7c5fa1302a837e95758e63/1', this.state.filter)
-            .then(this.onDataLoad)
-            .catch(this.onError);
-    }
-
-    onDataLoad = (data) => {
-        this.setState({
-            questionData: data,
-            loading: false
-        });
-    }
-
-    onError = () => {
-        this.setState({
-            error: true,
-            loading: false
-        });
-    };
-
-    onTabClick = (id) => {
-        this.setState({
-            currentActiveIndex: id
-        });
+        filter: ''
     }
 
     onSearchSubmit = (filter) => {
@@ -69,22 +20,12 @@ export default class App extends Component {
     }
 
     render() {
-        const { questionData, currentActiveIndex, loading, error } = this.state;
-
-        const hasData = !(loading || error);
-
-        const errorMessage = error ? <ErrorIndicator /> : null;
-        const spinner = loading ? <Spinner /> : null;
-        const content = hasData ? <Tabs questionData={questionData} onTabClick={this.onTabClick} activeTab={currentActiveIndex} /> : null;
-
         return (
             <ErrorBoundary>
                 <div className="app">
                     <AppHeader />
                     <SearchPanel onSearchSubmit={this.onSearchSubmit} />
-                    {errorMessage}
-                    {spinner}
-                    {content}
+                    <Tabs term={this.state.filter}/>
                 </div>
             </ErrorBoundary>
         );
--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -1,20 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './tabs.scss';
 
+import ApiService from '../../services/api-service';
 import TabList from '../tab-list';
 import TabContent from '../tab-content';
 import ErrorBoundary from '../error-boundary';
+import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator';
 
-const Tabs = ({ questionData, onTabClick, activeTab }) => {
-    return (
-        <ErrorBoundary>
+export default class Tabs extends Component {
+
+    apiService = new ApiService();
+
+    state = {
+        questionData: [],
+        currentActiveIndex: 0,
+        loading: true,
+        error: false
+    }
+
+    componentDidMount() {
+        this.updateData();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.term !== prevProps.term) {
+            this.setState({
+                loading: true
+            });
+            this.updateData();
+        }
+    }
+
+    updateData() {
+        this.apiService
+            .getData('5f7c5fa1302a837e95758e63/1', this.props.term)
+            .then(this.onDataLoad)
+            .catch(this.onError);
+    }
+
+    onDataLoad = (data) => {
+        this.setState({
+            questionData: data,
+            loading: false
+        });
+    }
+
+    onError = () => {
+        this.setState({
+            error: true,
+            loading: false
+        });
+    };
+
+    onTabClick = (id) => {
+        this.setState({
+            currentActiveIndex: id
+        });
+    }
+
+    render() {
+        const { questionData, currentActiveIndex, loading, error } = this.state;
+
+        const hasData = !(loading || error);
+
+        const errorMessage = error ? <ErrorIndicator /> : null;
+        const spinner = loading ? <Spinner /> : null;
+        const content = hasData ?
             <React.Fragment>
-                <TabList questionData={questionData} onTabClick={onTabClick} activeTab={activeTab} />
-                <TabContent questionData={questionData} activeTab={activeTab} />
-            </React.Fragment>
-        </ErrorBoundary>
-    );
+                <TabList questionData={questionData} onTabClick={this.onTabClick} activeTab={currentActiveIndex} />
+                <TabContent questionData={questionData} activeTab={currentActiveIndex} />
+            </React.Fragment> : null;
+
+        return (
+            <ErrorBoundary>
+                {errorMessage}
+                {spinner}
+                {content}
+            </ErrorBoundary>
+        );
+    };
 };
 
-export default Tabs;
